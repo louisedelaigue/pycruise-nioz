4319f1d8068d6cbbf8c4fedc8c7a61e9aa150346
Louise Delaigue <louisedelaigue@gmail.com>
2021-09-22 12:03:19 +0200

    finished

--- t0-homework.py
+++ t0-homework.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 # Import CTD bottles
-df0 = pd.read_csv('data/ctd-bottles-variants/00-ctd-bottles-2-1.csv', na_values=-999)
+df0 = pd.read_csv('data/ctd-bottles-variants/00-ctd-bottles-2-1.csv', 
+                  na_values=-999)
 
 df1 = pd.read_csv('data/ctd-bottles-variants/01-ctd-bottles-2-1.csv',
                   na_values=-999,
@@ -24,8 +25,21 @@ df4 = pd.read_table('data/ctd-bottles-variants/04-ctd-bottles-2-1.txt',
 
 df5 = pd.read_excel('data/ctd-bottles-variants/05-ctd-bottles-2-1.xlsx',
                   na_values=-999,
-                  skiprows=3)
+                  skiprows=[0,1,2,4])
 
+df6 = pd.read_excel('data/ctd-bottles-variants/06-ctd-bottles-2-1.xlsx',
+                  na_values=[-999, -777],
+                  skiprows=[0,1,2,4])
+
+df7 = pd.read_excel('data/ctd-bottles-variants/07-ctd-bottles-2-1.xlsx',
+                  na_values=[-999, -777],
+                  skiprows=[0,1,2,4])
+
+df8 = pd.read_csv('data/ctd-bottles-variants/08-ctd-bottles-2-1.csv',
+                  na_values=[-999, -777],
+                  skiprows=[0,1,2,4],
+                  skipfooter=3,
+                  engine='python')
 
 # Plot salinity profile
 df0.plot.scatter('salinity', 'depth')
@@ -34,3 +48,6 @@ df2.plot.scatter('salinity', 'depth')
 df3.plot.scatter('salinity', 'depth')
 df4.plot.scatter('salinity', 'depth')
 df5.plot.scatter('salinity', 'depth')
+df6.plot.scatter('salinity', 'depth')
+df7.plot.scatter('Practical salinity', 'Depth')
+df8.plot.scatter('Practical salinity', 'Depth')
